bca98990ee8c3916b966e3867955d34de64c7674
hmoustaphaousmane <hmoustaphaousmane@gmail.com>
2024-01-23 13:42:05 +0100

    init: initialize new Django project (structure and configuration)

---
+++ core/urls.py
@@ -0,0 +1,11 @@
+import os
+from dotenv import load_dotenv
+
+from django.contrib import admin
+from django.urls import path, include
+
+load_dotenv()
+
+urlpatterns = [
+    path(str (os.getenv ('ADMIN_URL')), admin.site.urls),
+]
