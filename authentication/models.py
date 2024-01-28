97e107ced38df17066236498f0d698a723aa0524
hmoustaphaousmane <hmoustaphaousmane@gmail.com>
2024-01-28 17:44:24 +0100

    feat: use default Django User model and generate Profile model

--- authentication/models.py
+++ authentication/models.py
@@ -1,15 +1,16 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import User
 
-class CustomUser(AbstractUser):
-    email_address = models.EmailField(unique=True)
-    username = models.CharField(max_length=150)
-    first_name = models.CharField(max_length=150)
-    last_name = models.CharField(max_length=150)
+class Profile(models.Model):
+    GENDER = (
+        ('F', 'Female'),
+        ('M', 'Male'),
+    )
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    adress = models.CharField(max_length=255)
+    gender = models.CharField(choices=GENDER, max_length=1)
+    birth_date = models.DateField()
     avatar = models.ImageField(upload_to='avatars/', blank=True, null=True)
 
-    USERNAME_FIELD = 'email_address'
-    REQUIRED_FIELDS = ['first_name', 'last_name']
-
     def __str__(self):
-        return self.email_address
+        return self.adress
