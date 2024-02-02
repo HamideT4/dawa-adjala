4d5b19e668401a3cef485b7f9164b4a3d5510db0
hmoustaphaousmane <hmoustaphaousmane@gmail.com>
2024-02-02 21:01:01 +0100

    chore: create and registre Hospital, Service, Consultation models

--- hospital/admin.py
+++ hospital/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Hospital, Service, Consultation
 
-# Register your models here.
+admin.site.register(Hospital)
+admin.site.register(Service)
+admin.site.register(Consultation)
