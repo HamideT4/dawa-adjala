dbb6c41555c8aec3fdec57314a25bf397d26e37f
HamideT4 <hamidemahamat20@gmail.com>
2024-02-20 08:16:19 +0100

    chore : add urls

--- core/urls.py
+++ core/urls.py
@@ -3,10 +3,15 @@ from dotenv import load_dotenv
 
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from .views import index
 
 load_dotenv()
 
 urlpatterns = [
     path(str (os.getenv ('ADMIN_URL')), admin.site.urls),
-    path('', include('authentication.urls')), # Authentication app URL
-]
+    path('', index, name='index'),
+    path('auth/', include('authentication.urls')), # Authentication app URL
+    path('hospital/', include('hospital.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
