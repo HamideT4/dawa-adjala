c02e824996b9ddc1c77f51566c28f4ba5e3ac828
HamideT4 <hamidemahamat20@gmail.com>
2024-08-06 18:04:49 +0100

    Turn debug on false and allow all hosts

--- core/settings.py
+++ core/settings.py
@@ -28,9 +28,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = str (os.getenv ('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['192.168.43.71','127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 SITE_ID = 1
 
